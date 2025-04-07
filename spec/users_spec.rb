354bf47ec4d0e737901f6c3d14d056af10fd646e
Roy Fredeluces <fredeluces.rcb@gmail.com>
2025-04-07 23:47:24 +0800

    removed test for disregarding case, added test for incorrect second argument type

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -12,21 +12,16 @@ describe UsersApp do
       expect{ (UsersApp.new.invoke(:findSubstr, [name, false, 'KEKL'])) }.to raise_error(Thor::InvocationError)
     end
 
+    it "the second argument to findSubstr method should be a boolean" do
+      expect{ (UsersApp.new.invoke(:findSubstr, [name, 'KEKL'])) }.to raise_error(TypeError)
+    end
+
     it "The findSubstr method should be empty if the case_sensitive flag is true even if the substring technically exists" do
       result = UsersApp.new.invoke(:findSubstr, [name.upcase, true])
       expect(result).to be_empty
     end
 
     it "the findSubstr method should return all records where the substr 'John' is in the record's name object" do
-      result = UsersApp.new.invoke(:findSubstr, [name])
-
-      result.each do|content|
-        expect(content['full_name'].downcase).to include(name.downcase)
-      end
-    end
-
-    it "the case should not matter" do
-      uppercase_name = name.upcase
       result = UsersApp.new.invoke(:findSubstr, [name])
 
       result.each do|content|
