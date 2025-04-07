a872a2607ec3a3f80f22e47a41b2eb9f13adaac9
RCBF <rcbf@192.168.1.9>
2025-04-07 18:33:57 +0800

    initial commit

---
+++ spec/users_spec.rb
@@ -0,0 +1,59 @@
+require "./users.rb"
+
+describe UsersApp do
+  context "testing the findSubstr method" do
+    let(:name) { "John" }
+
+    it "The findSubstr method should require a name variable" do
+      expect{ (UsersApp.new.invoke(:findSubstr, [])) }.to raise_error(Thor::InvocationError)
+    end
+    
+    it "The findSubstr method should not have more than two arguments" do
+      expect{ (UsersApp.new.invoke(:findSubstr, [name, false, 'KEKL'])) }.to raise_error(Thor::InvocationError)
+    end
+
+    it "The findSubstr method should be empty if the case_sensitive flag is true even if the substring technically exists" do
+      result = UsersApp.new.invoke(:findSubstr, [name.upcase, true])
+      expect(result).to be_empty
+    end
+
+    it "the findSubstr method should return all records where the substr 'John' is in the record's name object" do
+      result = UsersApp.new.invoke(:findSubstr, [name])
+
+      result.each do|content|
+        expect(content['full_name'].downcase).to include(name.downcase)
+      end
+    end
+
+    it "the case should not matter" do
+      uppercase_name = name.upcase
+      result = UsersApp.new.invoke(:findSubstr, [name])
+
+      result.each do|content|
+        expect(content['full_name'].downcase).to include(name.downcase)
+      end
+    end
+
+    it "the findSubstr method should return an empty array if there are no matches" do
+
+      result = UsersApp.new.invoke(:findSubstr, ["random_name"])
+
+      expect(result).to be_empty
+    end
+  end
+
+  context "testing the duplicates method" do
+    it "The duplicates method should not accept any parameters" do 
+      expect{ (UsersApp.new.invoke(:duplicates, ['s'])) }.to raise_error(Thor::InvocationError)
+    end
+
+    it "The duplicates method returns the objects which have the same email address" do 
+      result = UsersApp.new.invoke(:duplicates, [])
+      result1 = result[0]['email']
+
+      result.each do|content|
+        expect(content['email']).to eq(result1)
+      end
+    end
+  end
+end
---
+++ users.rb
@@ -0,0 +1,55 @@
+require 'thor'
+require 'net/http'
+require 'uri'
+require 'json'
+
+class UsersApp < Thor
+
+    desc "hello NAME", "echoes Hello World"
+    def hello(name)
+        p "Hello World"
+    end
+
+    desc 'findSubstr substr', 'Finds Users with the substr in name'  
+    def findSubstr(name, case_sensitive=false)
+
+        subs = !case_sensitive ? name.downcase : name
+        users = [] 
+
+        pageContent = fetchData
+
+        pageContent.map do |content|
+            users << content if (case_sensitive ? (content["full_name"].include? subs) : (content["full_name"].downcase.include? subs))
+        end
+
+        p users
+    end
+
+    desc 'duplicates', 'Finds users with same email addresses'
+    def duplicates
+
+        emails = []
+        dups = []
+
+        pageContent = fetchData
+
+        emails << pageContent.map { |content| content['email'] }
+            
+        duplicate_emails = emails[0].group_by(&:itself).select { |k, v| v.size > 1 }.keys
+
+        duplicate_emails.map { |email| pageContent.map { |content| dups << content if content['email'] === email } }
+
+        p dups
+
+    end
+
+    private
+
+    def fetchData
+        JSON.parse(Net::HTTP.get(URI.parse('https://appassets02.shiftcare.com/manual/clients.json')))
+    end
+
+end
+
+UsersApp.start(ARGV)
+
